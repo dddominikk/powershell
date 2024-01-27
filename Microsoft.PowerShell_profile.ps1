4b0b61633fad53f33459dbed552f69fcdf4f1987
Dominik Bošnjak <bosnjakinimod@gmail.com>
2024-01-27 13:31:48 +0100

    added bd function that goes back to the previous directory

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,25 @@
 ﻿Set-StrictMode -Version 5;
+[System.Collections.Stack]$GLOBAL:dirStack = @()
+$GLOBAL:oldDir = ''
+$GLOBAL:addToStack = $true
+function prompt
+{
+    Write-Host "PS $(get-location)>"  -NoNewLine #-foregroundcolor Magenta
+    $GLOBAL:nowPath = (Get-Location).Path
+    if(($nowPath -ne $oldDir) -AND $GLOBAL:addToStack){
+        $GLOBAL:dirStack.Push($oldDir)
+        $GLOBAL:oldDir = $nowPath
+    }
+    $GLOBAL:AddToStack = $true
+    return ' '
+}
+function BackOneDir{
+    $lastDir = $GLOBAL:dirStack.Pop()
+    $GLOBAL:addToStack = $false
+    cd $lastDir
+}
+
+Set-Alias bd BackOneDir
 
 function Invoke-Utility {
     <#
